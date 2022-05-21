99f2893fd526ef9c0bc03c0c9fa3acd72be5085c
OsvalDM <58128208+OsvalDM@users.noreply.github.com>
2022-05-20 21:06:06 -0500

    Agregar musica

--- scripts.js
+++ scripts.js
@@ -22,6 +22,9 @@ ScrollReveal().reveal('.containerChurch__containerText', {duration: 1200,distanc
 ScrollReveal().reveal('.containerAlbum__title', {duration: 1200,distance: '80px',delay:400});
 ScrollReveal().reveal('.containerAlbum__img', {duration: 1200,distance: '50px',delay:400});
 
+ScrollReveal().reveal('.containerConfirmation__containerImg', {duration: 1200,distance: '80px',delay:400});
+ScrollReveal().reveal('.containerConfirmation__iframe', {duration: 1200,distance: '50px',delay:400});
+
 ScrollReveal().reveal('.containerFooter', {duration: 1200,distance: '80px',delay:100});
 
 //cuenta regresiva
@@ -31,21 +34,14 @@ const SPAN_DAYS = document.querySelector('h2#days');
 const SPAN_HOURS = document.querySelector('h2#hours');
 const SPAN_MINUTES = document.querySelector('h2#minutes');
 const SPAN_SECONDS = document.querySelector('h2#seconds');
+
 // Milliseconds for the calculations
 const MILLISECONDS_OF_A_SECOND = 1000;
 const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
 const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
-const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24
+const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24;
 
-//===
-// FUNCTIONS
-//===
-
-/**
- * Method that updates the countdown and the sample
- */
-function updateCountdown() {
-    // Calcs
+function updateCountdown() {    
     const NOW = new Date()
     const DURATION = DATE_TARGET - NOW;
     const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
@@ -54,18 +50,32 @@ function updateCountdown() {
     const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
     // Thanks Pablo Monteserín (https://pablomonteserin.com/cuenta-regresiva/)
 
-    // Render
     SPAN_DAYS.textContent = REMAINING_DAYS;
     SPAN_HOURS.textContent = REMAINING_HOURS;
     SPAN_MINUTES.textContent = REMAINING_MINUTES;
     SPAN_SECONDS.textContent = REMAINING_SECONDS;
 }
 
-//===
-// INIT
-//===
 updateCountdown();
-// Refresh every second
 setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
 
+//imagen
+
+const image = document.getElementById("bg");
+let numeroAnterior = 0;
+let n = 0;
+
+function changeSource(){    
+    do{
+        n = parseInt(Math.random() * 4 + 1);
+    }while(n==numeroAnterior);
+    numeroAnterior = n;
+    image.src="./img/n"+n+".jpg";
+    console.log("./img/n"+n+".jpg");
+}
+
+setInterval(changeSource, MILLISECONDS_OF_A_SECOND*10);
+
 //audio
+let audioEtiqueta = document.querySelector("audio")
+audioEtiqueta.play();
